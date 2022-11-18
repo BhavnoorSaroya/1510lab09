8a264a52072460acd66107c9a929080cc620270b
Bhavnoor Saroya <bsaroya3@my.bcit.ca>
2022-11-17 20:42:12 -0800

    fixed the cathoteldriver

--- Comp1510Lab09SaroyaB/src/ca/bcit/comp1510/lab09/CatHotelDriver.java
+++ Comp1510Lab09SaroyaB/src/ca/bcit/comp1510/lab09/CatHotelDriver.java
@@ -3,63 +3,46 @@
  */
 package ca.bcit.comp1510.lab09;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Random;
 
 /**
  * @author saroy
  *
  */
-public class CatHotel {
+public class CatHotelDriver {
 
-    
-    
-    String hotelName;
-    ArrayList<Cat> cats = new ArrayList<Cat>();
-    
-    public CatHotel(String name) {
-        hotelName = name;
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        CatHotel testHotel = new CatHotel("Test Hotel");
+        
+        Random randnum = new Random();
+        
+        Cat cat1 = new Cat(randnum.nextInt(10), "TestCat1");
+        Cat cat2 = new Cat(randnum.nextInt(10), "TestCat2");
+        Cat cat3 = new Cat(randnum.nextInt(10), "TestCat3");
+        Cat cat4 = new Cat(randnum.nextInt(10), "TestCat4");
+        
+        CatHotel hotelOfCats = new CatHotel("hotelOfCats");
+        
+        hotelOfCats.addCat(cat1);
+        hotelOfCats.addCat(cat2);
+        hotelOfCats.addCat(cat3);
+        hotelOfCats.addCat(cat4);
+        
+        System.out.println("Number of guests: " + hotelOfCats.guestCount());
+        
+        hotelOfCats.printGuestList();
+        
+        hotelOfCats.removeOldGuests(2);
+        System.out.println("Guests older than age 2 removed, new guest list:");
+        
+        hotelOfCats.printGuestList();
+        
+        
+        
         
     }
-    
-    public void addCat(Cat cat) {
-        
-        cats.add(cat);
-    }
-    
-    public void removeAllGuests() {
-        cats.removeAll(cats);
-    }
-    
-    public int guestCount() {
-        return cats.size();
-    }
-    
-    
-    public int removeOldGuests(int age) {
-        int catsRemoved = 0;
-        Iterator<Cat> catIterator = cats.iterator();
-        
-        
-        
-        while (catIterator.hasNext()) {
-            Cat temp = catIterator.next();
-            
-            if (temp.getAge() > age){
-                catIterator.remove();
-                catsRemoved++;
-            }
-        }
-        return catsRemoved;
-    }
-    
-    
-    public void printGuestList() {
-        System.out.println(hotelName);
-        for (Cat i : cats) {
-            System.out.println(i.toString());
-            
-            
-        }
-    }
+
 }
